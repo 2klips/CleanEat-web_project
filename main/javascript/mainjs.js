0e7d8849d499e2e593e9f318ed9dc3c915018842
Songysp <hyeon9502@naver.com>
2024-04-29 16:34:50 +0900

    로그인이false일경우 로그인 페이지로 true일경우 마이페이지로 갈수있게 수정

--- main/javascript/mainjs.js
+++ main/javascript/mainjs.js
@@ -1,3 +1,4 @@
+const logincheck = false
 window.navermap_authFailure = function () {
     // 인증 실패 시 처리 코드 작성
     alert('클라이언트 인증 실패!')
@@ -10,3 +11,12 @@ function checkfilter() {
         document.getElementsByClassName('filter').checked = true;
     }
 };
+
+function changeherf(){
+    loginbtn = document.getElementById("loginbtn");
+    if(logincheck){
+        loginbtn.href = "../info/main.html";
+    }else{
+        loginbtn.href = "../login regist/index.html";
+    }
+}
