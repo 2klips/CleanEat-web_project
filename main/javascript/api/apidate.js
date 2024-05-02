bb3aac83d3747db5c8d3cfa2bb9c0edf3ca95cc4
Songysp <hyeon9502@naver.com>
2024-05-02 17:45:46 +0900

    api리스트

---
+++ main/javascript/api/apidate.js
@@ -0,0 +1,82 @@
+let apidate = {
+    // 강북구 모범음식점 (지정현황) 조회
+    gangbuk:`http://openAPI.gangbuk.go.kr:8088/${apikey}/json/GbModelRestaurantDesignate/${startindex}/${endindex}/;`,
+
+    // 도봉구 모범음식점 (지정현황) 조회
+    dobong:`http://openAPI.dobong.go.kr:8088/${apikey}/json/DobongModelRestaurantDesignate/${startindex}/${endindex}/`,
+
+    // 용산구 모범음식점 (지정현황) 조회
+    yongsan:`http://openAPI.yongsan.go.kr:8088/${apikey}/json/YsModelRestaurantDesignate/${startindex}/${endindex}/`,
+
+    // 성동구 모범음식점 (지정현황) 조회
+    sd:`http://openAPI.sd.go.kr:8088/${apikey}/json/SdModelRestaurantDesignate/${startindex}/${endindex}/`,
+
+    // 성북구 모범음식점 (지정현황) 조회
+    sb:`http://openAPI.sb.go.kr:8088/${apikey}/json/SbModelRestaurantDesignate/${startindex}/${endindex}/`,
+
+    // 광진구 모범음식점 (지정현황) 조회
+    gwangjin:`http://openAPI.gwangjin.go.kr:8088/${apikey}/json/GwangjinModelRestaurantDesignate/${startindex}/${endindex}/`,
+
+    // 마포구 모범음식점 (지정현황) 조회
+    mapo:`http://openAPI.mapo.go.kr:8088/${apikey}/json/MpModelRestaurantDesignate/${startindex}/${endindex}/`,
+
+    // 은평구 모범음식점 (지정현황) 조회
+    ep:`http://openAPI.ep.go.kr:8088/${apikey}/json/EpModelRestaurantDesignate/${startindex}/${endindex}/`,
+
+    // 중랑구 모범음식점 (지정현황) 조회
+    jungnang:`http://openAPI.jungnang.seoul.kr:8088/${apikey}/json/JungnangModelRestaurantDesignate/${startindex}/${endindex}/`,
+
+    // 강동구 모범음식점 (지정현황) 조회
+    gd:`http://openAPI.gd.go.kr:8088/${apikey}/json/GdModelRestaurantDesignate/${startindex}/${endindex}/`,
+
+    // 관악구 모범음식점 (지정현황) 조회
+    gwanak:`http://openAPI.gwanak.go.kr:8088/${apikey}/json/GaModelRestaurantDesignate/${startindex}/${endindex}/`,
+
+    // 서초구 모범음식점 (지정현황) 조회
+    seocho:`http://openAPI.seocho.go.kr:8088/${apikey}/json/ScModelRestaurantDesignate/${startindex}/${endindex}/`,
+
+    // 종로구 모범음식점 (지정현황) 조회
+    jongno:`http://openAPI.jongno.go.kr:8088/${apikey}/json/JongnoModelRestaurantDesignate/${startindex}/${endindex}/`,
+
+    // 양천구 모범음식점 (지정현황) 조회
+    yangcheon:`http://openAPI.yangcheon.go.kr:8088/${apikey}/json/YcModelRestaurantDesignate/${startindex}/${endindex}/`,
+
+    // 구로구 모범음식점 (지정현황) 조회
+    guro:`http://openAPI.guro.go.kr:8088/${apikey}/json/GuroModelRestaurantDesignate/${startindex}/${endindex}/`,
+
+    // 동대문구 모범음식점 (지정현황) 조회
+    ddm:`http://openAPI.ddm.go.kr:8088/${apikey}/json/DongdeamoonModelRestaurantDesignate/${startindex}/${endindex}/`,
+
+    // 동작구 모범음식점 (지정현황) 조회
+    dongjak:`http://openAPI.dongjak.go.kr:8088/${apikey}/json/DjModelRestaurantDesignate/${startindex}/${endindex}/`,
+
+    // 중구 모범음식점 (지정현황) 조회
+    junggu:`http://openAPI.junggu.seoul.kr:8088/${apikey}/json/JungguModelRestaurantDesignate/${startindex}/${endindex}/`,
+
+    // 노원구 모범음식점 (지정현황) 조회
+    nowon:`http://openAPI.nowon.go.kr:8088/${apikey}/json/NwModelRestaurantDesignate/${startindex}/${endindex}/`,
+
+    // 금천구 모범음식점 (지정현황) 조회
+    geumcheon:`http://openAPI.geumcheon.go.kr:8088/${apikey}/json/GeumcheonModelRestaurantDesignate/${startindex}/${endindex}/`,
+
+    // 영등포구 모범음식점 (지정현황) 조회
+    ydp:`http://openAPI.ydp.go.kr:8088/${apikey}/json/YdpModelRestaurantDesignate/${startindex}/${endindex}/`,
+
+    // 서대문구 모범음식점 (지정현황) 조회
+    sdm:`http://openAPI.sdm.go.kr:8088/${apikey}/json/SeodaemunModelRestaurantDesignate/${startindex}/${endindex}/`,
+
+    // 강남구 모범음식점 (지정현황) 조회
+    gangnam:`http://openAPI.gangnam.go.kr:8088/${apikey}/json/GnModelRestaurantDesignate/${startindex}/${endindex}/`,
+
+    // 강서구 모범음식점 (지정현황) 조회
+    gangseo:`http://openAPI.gangseo.seoul.kr:8088/${apikey}/json/GangseoModelRestaurantDesignate/${startindex}/${endindex}/`,
+
+    // 송파구 모범음식점 (지정현황) 조회
+    songpa:`http://openAPI.songpa.seoul.kr:8088/${apikey}/json/SpModelRestaurantDesignate/${startindex}/${endindex}/`,
+
+    // 서울시 위생업소 전체 행정처분내역 현황
+    violation:`http://openapi.seoul.go.kr:8088/${apikey}/json/SeoulAdminMesure/${startindex}/${endindex}/`,
+
+    // 식품접객업소 위생등급 지정현황
+    saferanked:`http://openapi.foodsafetykorea.go.kr/api/${apikey}/C004/json/${startindex}/${endindex}/`,
+}
