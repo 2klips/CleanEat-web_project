eb33fb40460ef16025d6dc45cf40d95cb6f3aad2
Songysp <156406181+Songysp@users.noreply.github.com>
2024-05-13 18:47:52 +0900

    Revert "로그인 페이지 백앤드 적용중"
    This reverts commit 4cc881c7cc358de3074c34f5bb42bb6b70cb1e96.

--- server/controller/users.js
+++ server/controller/users.js
@@ -1,52 +1,8 @@
-const userDB = require('../database/userDB.js');
+const database = require('../database/database.js');
 const config = require('../config.js');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-
-
-function createJwtToken(id){
-    return jwt.sign({id}, config.jwt.secretKey, {expiresIn: config.jwt.expiresInSec});
-}
-
-
-async function signup(req, res, next){
-    let {name, password, email, addr1, addr2, hp, nickname} = req.body;
-    const found = await userDB.findByEmail(email);
-    if(found){
-        return res.status(409).json({message:`${username}이 이미 있습니다`});
-    }
-    password = await bcrypt.hash(password, config.bcrypt.saltRounds);
-    const userInfo = await userDB.createUser({name, password, email, addr1, addr2, hp, nickname});
-    const token = createJwtToken(userInfo);
-    res.status(201).json({token, nickname});
-}
-
-
-async function login(req, res, next){
-    const {email, password} = req.body;
-    // const user = await userDB.login(username);
-    const user = await userDB.findByEmail(email);
-    console.log(user);
-    if(!user){
-        return res.status(401).json({message: `아이디를 찾을 수 없음`});
-    }
-    const isValidpassword = await bcrypt.compareSync(password, user.password);
-    if(!isValidpassword){
-        return res.status(401).json({message: `비밀번호가 틀렸음`});
-    }
-    const token = createJwtToken(user.email);
-    res.status(200).json({token, nickname});
-}
-
-
-async function me(req, res, next){
-    const user = await userDB.findByEmail(email);
-    if(!user){
-        return res.status(404).json({message: `일치하는 사용자가 없음`});
-    }
-    res.status(200).json({token: req.token, nickname: user.nickname});
-}
 
 
 
-module.exports = {signup, login, me};
+
+
+module.exports = {};
--- server/middleware/middleware.js
+++ server/middleware/middleware.js
@@ -1,35 +1,33 @@
 const jwt = require('jsonwebtoken');
-const userDB = require('../database/userDB.js');
-const config = require('../config.js');
+const database = require('../database/database');
+
 
 const AUTH_ERROR = {message: "인증에러"};
 
 
 const isAuth = async (req, res, next) => {
     const authHeader = req.get('Authorization');
-    if(!authHeader){
-        return res.sendFile(path.join(__dirname, '../public/login_regis/index.html'));
-    }
-    if(authHeader.startsWith('Bearer ')){
+    console.log(authHeader);
+    if(!(authHeader && authHeader.startsWith('Bearer '))){
         console.log('에러1');
         return res.status(401).json(AUTH_ERROR);
     }
     const token = authHeader.split(' ')[1];
     jwt.verify(
-        token, config.jwt.secretKey, async(error, decoded) => {
+        token, 'abcd1234%^&*', async(error, decoded) => {
             if(error){
                 console.log('에러2');
                 return res.status(401).json(AUTH_ERROR);
             }
-            const user = await userDB.findByEmail(decoded.email);
+            const user = await database.findById(decoded.id);
             if(!user){
                 console.log('에러3');
                 return res.status(401).json(AUTH_ERROR);
             }
-            req.userEmail = user.email;
+            req.userId = user.id;
             next();
         }
     );
 }
 
-module.exports = {isAuth};
+module.exports = isAuth;
