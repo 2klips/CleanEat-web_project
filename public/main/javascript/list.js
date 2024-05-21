d7f58e119ae32dc2b97f3d57a0846df4aec9ee5a
2klips <axz1420@naver.com>
2024-05-22 02:45:12 +0900

    ## list.js 수정중 ##

--- public/main/javascript/list.js
+++ public/main/javascript/list.js
@@ -1,202 +1,170 @@
-const searchBtn = document.getElementById('search-btn');
-const searchbox = document.getElementById('search');
-
-/**
- * 데이터를 화면에 표시하는 함수
- * @param {Object} datas 화면에 표시할 데이터 객체
- */
-async function displayData(datas) {
-    const container = document.getElementById('data-container');
-    container.innerHTML = ''; // 이전 데이터를 지웁니다.
-
-    if (datas && Array.isArray(datas)) {
-        datas.forEach(item => {
-            let rank = '';
-            if(item.rank == '매우우수'){
-                rank = `<div class="star"><h3>위생등급: 매우우수<img src="./css/images/3star.svg" class="3star"></h3></div>`;
-            } else if(item.rank == '우수'){
-                rank = `<div class="star"><h3>위생등급: 우수<img src="./css/images/2star.svg" class="2star"></h3></div>`;
-            } else if(item.rank == '좋음'){
-                rank = `<div class="star"><h3>위생등급: 좋음<img src="./css/images/1star.svg" class="1star"></h3></div>`;
-            }
-
-            if (!item.detail){
-                item.detail = '';
-            } else {
-                item.detail = `위반내용: ` + item.detail;
-            }
-            if (!item.no){
-                item.no = '';
-            } else {
-                item.no = `지정번호: ` + item.no;
-            }
-            if (!item.penalty){
-                item.penalty = '';
-            } else {
-                item.penalty = `처벌내용: ` + item.penalty;
-            }
-            if (!item.category){
-                item.category = '';
-            } else {
-                item.category = `업종명: ` + item.category;
-            }
-
-            // 전화번호 처리
-            let tel = item.tel || '';
-            if (tel.includes('*')) {
-                tel = '';
-            } else {
-                tel = tel.replace(/\s+/g, '-'); // 숫자 사이에 빈칸이 있으면 "-"로 대체
-                if (tel.startsWith('02')) {
-                    if (tel.length === 9) {
-                        tel = tel.replace(/(\d{2})(\d{3})(\d{4})/, '$1-$2-$3');
-                    } else {
-                        tel = tel.replace(/(\d{2})(\d{4})(\d{4})/, '$1-$2-$3');
-                    }
-                } else if (tel.length <= 8) {
-                    tel = '';
-                }
-            }
-
-            const itemElement = document.createElement('div');
-            itemElement.classList.add('content');
-            if (item.detail) {
-                itemElement.classList.add('violation');
-            }
-            let itemHTML = '<div class="content-info">';
-            itemHTML += `<h2>${item.name}`;
-
-            if (item.detail) {
-                itemHTML += ` <img src="./css/images/violation_icon.png" alt="위반" class="violation-icon">`;
-            } else if (!item.detail && !item.rank) {
-                itemHTML += ` <span class="exemplary-text"><img src="./css/images/exemplary_icon.png" alt="모범음식점" class="exemplary-icon"> 모범음식점</span>`;
-            }
-
-            itemHTML += `</h2>`;
-            itemHTML += rank; // 위생등급
-            itemHTML += `<p>${item.detail || ''}</p><br>`;                                                    
-            itemHTML += `<p>지정일자: ${item.date || ''}</p><br>`; // 지정일자
-            itemHTML += `<p>${item.addr || ''}</p><br>`; // 주소
-            if (tel) {
-                itemHTML += `<p>전화번호: ${tel}</p><br>`; // 전화번호
-            }
-            itemElement.innerHTML = itemHTML;
-            container.appendChild(itemElement);
-        });
-    } else {
-        console.error('데이터가 없습니다.');
-    }
-}
-
-// 페이지 로드 시 sessionStorage에서 검색 결과 복원
 document.addEventListener('DOMContentLoaded', function() {
+    const searchBtn = document.getElementById('search-btn');
+    const searchbox = document.getElementById('search');
+
+    async function displayListData(datas) {
+        const container = document.getElementById('data-container');
+        container.innerHTML = ''; // 이전 데이터를 지웁니다.
+
+        if (datas && Array.isArray(datas)) {
+            datas.forEach(item => {
+                let rank = '';
+                if(item.rank === '매우우수'){
+                    rank = `<div class="star"><h3>위생등급: 매우우수<img src="./css/images/3star.svg" class="3star"></h3></div>`;
+                } else if(item.rank === '우수'){
+                    rank = `<div class="star"><h3>위생등급: 우수<img src="./css/images/2star.svg" class="2star"></h3></div>`;
+                } else if(item.rank === '좋음'){
+                    rank = `<div class="star"><h3>위생등급: 좋음<img src="./css/images/1star.svg" class="1star"></h3></div>`;
+                }
+
+                if (!item.detail){
+                    item.detail = '';
+                } else {
+                    item.detail = `위반내용: ` + item.detail;
+                }
+                if (!item.no){
+                    item.no = '';
+                } else {
+                    item.no = `지정번호: ` + item.no;
+                }
+                if (!item.penalty){
+                    item.penalty = '';
+                } else {
+                    item.penalty = `처벌내용: ` + item.penalty;
+                }
+                if (!item.category){
+                    item.category = '';
+                } else {
+                    item.category = `업종명: ` + item.category;
+                }
+
+                let tel = item.tel || '';
+                if (tel.includes('*')) {
+                    tel = '';
+                } else {
+                    tel = tel.replace(/\s+/g, '-'); // 숫자 사이에 빈칸이 있으면 "-"로 대체
+                    if (tel.startsWith('02')) {
+                        if (tel.length === 9) {
+                            tel = tel.replace(/(\d{2})(\d{3})(\d{4})/, '$1-$2-$3');
+                        } else {
+                            tel = tel.replace(/(\d{2})(\d{4})(\d{4})/, '$1-$2-$3');
+                        }
+                    } else if (tel.length <= 8) {
+                        tel = '';
+                    }
+                }
+
+                const itemElement = document.createElement('div');
+                itemElement.classList.add('content');
+                if (item.detail) {
+                    itemElement.classList.add('violation');
+                }
+                let itemHTML = '<div class="content-info">';
+                itemHTML += `<h2>${item.name}`;
+
+                if (item.detail) {
+                    itemHTML += ` <img src="./css/images/violation_icon.png" alt="위반" class="violation-icon">`;
+                } else if (!item.detail && !item.rank) {
+                    itemHTML += ` <span class="exemplary-text"><img src="./css/images/exemplary_icon.png" alt="모범음식점" class="exemplary-icon"> 모범음식점</span>`;
+                }
+
+                itemHTML += `</h2>`;
+                itemHTML += rank; // 위생등급
+                itemHTML += `<p>${item.detail || ''}</p><br>`;                                                    
+                itemHTML += `<p>지정일자: ${item.date || ''}</p><br>`; // 지정일자
+                itemHTML += `<p>${item.addr || ''}</p><br>`; // 주소
+                if (tel) {
+                    itemHTML += `<p>전화번호: ${tel}</p><br>`; // 전화번호
+                }
+                itemElement.innerHTML = itemHTML;
+                container.appendChild(itemElement);
+            });
+        } else {
+            console.error('데이터가 없습니다.');
+        }
+    }
+
     const savedResults = sessionStorage.getItem('searchResults');
     if (savedResults) {
         const results = JSON.parse(savedResults);
-        displayData(results);
+        displayListData(results);
     }
-});
 
-async function search() {
-    scrollToTop();
-    const keyword = searchbox.value;
-    const checkboxes = document.querySelectorAll('.search-check:checked');
-    const rank = document.querySelectorAll('.rank:checked');
-    const ubtype = document.querySelectorAll('.ubtype:checked');
-    const collection = [];
-    checkboxes.forEach(checkbox => {
-        collection.push(checkbox.value);
-    });
-    if (collection.length >= 2) {
-        console.error('위생등급은 하나만 선택하세요.');
-        alert('위생등급은 하나만 선택하세요.');
-        return;
-    }
-    if (!keyword) {
-        console.error('검색어를 입력하세요.');
-        alert('검색어를 입력하세요.');
-        return;
-    }
-    let types = [];
-    let rans = [];
-    rank.forEach(rank => {
-        rans.push(rank.value);
-    });
-    ubtype.forEach(type => {
-        types.push(type.value);
-    });
-    let query = {
-        $and: [
-            {
-                $or: [
-                    { addr: { $regex: keyword, $options: 'i' } }, // 'i' 옵션은 대소문자 구분 없이 검색하도록 합니다.
-                    { name: { $regex: keyword, $options: 'i' } }
-                ]
-            },
-        ]
-    };
-    if (rans.length > 0) {
-        query.$and.push({ rank: { $in: rans } });
-    }
-    if (types.length > 0) {
-        if (types.length > 0) {
-            if (types.includes('음식점')) {
-                // '음식점'이 types 배열 안에 포함되어 있는 경우
-                query.$and.push({ type: { $not: { $regex: '급식',$options: 'i' } }, 
-                category: { $not: { $regex: '급식',$options: 'i' } } });  
-            } else if (types.includes('급식')) {
-                // '급식'이 types 배열 안에 포함되어 있는 경우
-                query.$and.push({ 
-                    $or: [
-                        { type: { $regex: '급식', $options: 'i' } },
-                        { category: { $regex: '급식', $options: 'i' } }
-                    ]
-                });
+    if (searchBtn) {
+        searchBtn.addEventListener('click', async function() {
+            if (searchBtn.disabled) {
+                console.log("서버 통신 중");
+                return;
             }
-        }
-    } else if (types.length >= 2){
-        return alert('분류를 하나만 선택해주세요');
-    };
-    console.log(query, collection);
-    try {
-        const queryString = encodeURIComponent(JSON.stringify(query));
-        const response = await fetch(`/main/search?collection=${collection}&query=${queryString}`);
-        if (!response.ok) {
-            throw new Error('서버 응답에 문제가 발생했습니다.');
-        }
-
-        const data = await response.json();
-        console.log(data);
-        displayData(data);
-    } catch (error) {
-        console.error('데이터를 불러오는 중 오류가 발생했습니다.', error);
+            searchBtn.disabled = true;
+            await window.search('data-container');
+            displayListData(JSON.parse(sessionStorage.getItem('searchResults')));
+            searchBtn.disabled = false;
+        });
     }
-}
 
-function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-}
-
-// 검색 버튼 클릭 시 실행되는 이벤트 리스너
-searchBtn.addEventListener('click', async function() {
-    if(searchBtn.disabled) {
-        console.log("서버 통신 중");
-        return;
-    };
-    searchBtn.disabled = true;
-    await search();
-    searchBtn.disabled = false;
-});
-
-// 검색어 입력 필드에서 Enter 키 입력 시 실행되는 이벤트 리스너
-searchbox.addEventListener('keypress', async function(event) {
-    if (searchBtn.disabled) {
-        console.log("서버 통신 중");
-        return;
-    };
-    searchBtn.disabled = true;
-    if (event.key === 13) {
-        await search();
+    if (searchbox) {
+        searchbox.addEventListener('keypress', async function(event) {
+            if (event.key === 'Enter') {
+                if (searchBtn.disabled) {
+                    console.log("서버 통신 중");
+                    return;
+                }
+                searchBtn.disabled = true;
+                await window.search('data-container');
+                displayListData(JSON.parse(sessionStorage.getItem('searchResults')));
+                searchBtn.disabled = false;
+            }
+        });
     }
-    searchBtn.disabled = false;
+
+    // window.search 함수를 정의합니다.
+    window.search = async function(containerId = 'data-container') {
+        const keyword = searchbox.value;
+        const checkboxes = document.querySelectorAll('.search-check:checked');
+        const rank = document.querySelectorAll('.rank:checked');
+        const collection = [];
+        checkboxes.forEach(checkbox => {
+            collection.push(checkbox.value);
+        });
+        if (!keyword) {
+            console.error('검색어를 입력하세요.');
+            alert('검색어를 입력하세요.');
+            return;
+        }
+        let rans = [];
+        rank.forEach(rank => {
+            rans.push(rank.value);
+        });
+        let query = {
+            $and: [
+                {
+                    $or: [
+                        { addr: { $regex: keyword, $options: 'i' } },
+                        { name: { $regex: keyword, $options: 'i' } }
+                    ]
+                },
+            ]
+        };
+        if (rans.length > 0) {
+            query.$and.push({ rank: { $in: rans } });
+        }
+        try {
+            const queryString = encodeURIComponent(JSON.stringify(query));
+            const response = await fetch(`http://localhost:8080/main/search?collection=${collection}&query=${queryString}`);
+            if (!response.ok) {
+                throw new Error('서버 응답에 문제가 발생했습니다.');
+            }
+            const data = await response.json();
+
+            sessionStorage.setItem('searchResults', JSON.stringify(data));
+            sessionStorage.setItem('searchKeyword', keyword);
+            sessionStorage.setItem('searchCollection', JSON.stringify(collection));
+            sessionStorage.setItem('searchRank', JSON.stringify(rans));
+
+            displayListData(data);
+        } catch (error) {
+            console.error('데이터를 불러오는 중 오류가 발생했습니다.', error);
+        }
+    };
 });
--- public/main/javascript/mainjs.js
+++ public/main/javascript/mainjs.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-    // 페이지 새로고침 여부 확인
-    if (performance.navigation.type === 1) { // 1은 새로고침을 의미함
+    if (performance.navigation.type === 1) {
         localStorage.removeItem('searchResults');
         localStorage.removeItem('searchKeyword');
         localStorage.removeItem('searchCollection');
@@ -17,15 +15,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const searchBtn = document.getElementById('search-btn');
     const searchbox = document.getElementById('search');
 
-    // 데이터를 화면에 표시하는 함수
-    async function displayData(datas) {
-        const container = document.getElementById('slide');
+    async function displayData(datas, containerId = 'slide') {
+        const container = document.getElementById(containerId);
+        
         container.innerHTML = ''; // 이전 데이터를 지웁니다.
         const empty = document.createElement('div');
         empty.id = 'slide-empty';
         container.appendChild(empty);
 
-        clearMarkersAndOverlays(); // 기존 마커와 오버레이 제거
+        // 마커와 오버레이 제거 함수 호출
+        clearMarkersAndOverlays();
 
         const addresses = [];
 
@@ -45,7 +44,6 @@ document.addEventListener('DOMContentLoaded', function() {
                         violation: item.detail ? true : false
                     });
 
-                    // Rank에 따른 이미지 선택
                     let starImg = '';
                     if (item.rank === '매우우수') {
                         starImg = './css/images/3star.svg';
@@ -55,7 +53,6 @@ document.addEventListener('DOMContentLoaded', function() {
                         starImg = './css/images/1star.svg';
                     }
 
-                    // 전화번호 처리
                     let tel = item.tel || '';
                     if (tel.includes('*')) {
                         tel = '';
@@ -72,15 +69,12 @@ document.addEventListener('DOMContentLoaded', function() {
                         }
                     }
 
-                   // slide에 데이터 출력
                     const slideItem = document.createElement('div');
                     slideItem.classList.add('slide-content');
 
-                    // detail 데이터가 있는 경우, 별도의 클래스를 추가
                     if (item.detail) {
                         slideItem.classList.add('violation');
                     }
-
 
                     let slideContent = `<h2>${item.name}`;
 
@@ -103,7 +97,6 @@ document.addEventListener('DOMContentLoaded', function() {
                     if (item.detail) {
                         slideContent += `<p id="detail">위반내용 : ${item.detail}</p>`;
                     }
-                    
 
                     slideItem.innerHTML = slideContent;
                     container.appendChild(slideItem);
@@ -114,25 +107,14 @@ document.addEventListener('DOMContentLoaded', function() {
             const sortedAddresses = sortResultsByDistance(addresses, center);
             const limitedAddresses = sortedAddresses.slice(0, MAX_MARKERS + zoomLevel * 5);
 
-            // localStorage에 마커와 인포윈도우 데이터를 저장
             localStorage.setItem('addresses', JSON.stringify(limitedAddresses));
             addMarkers(limitedAddresses, true);
-
         } else {
             console.error('데이터가 없습니다.');
         }
     }
 
-    // 마커를 추가하는 함수
-    function addMarkers(addressData, shouldRecenter) {
-        clearMarkersAndOverlays();
-        addressData.forEach((data, index) => {
-            searchAndDisplayAddress(data, shouldRecenter && index === 0);
-        });
-    }
-
-    // 검색어를 서버에 전송하여 데이터를 검색하는 함수
-    async function search() {
+    async function search(containerId = 'slide') {
         scrollToTop();
         const keyword = searchbox.value;
         const checkboxes = document.querySelectorAll('.search-check:checked');
@@ -171,26 +153,30 @@ document.addEventListener('DOMContentLoaded', function() {
             }
             const data = await response.json();
 
-            // 검색 결과와 조건을 sessionStorage에 저장
             sessionStorage.setItem('searchResults', JSON.stringify(data));
             sessionStorage.setItem('searchKeyword', keyword);
             sessionStorage.setItem('searchCollection', JSON.stringify(collection));
             sessionStorage.setItem('searchRank', JSON.stringify(rans));
 
-            // 화면에 데이터를 표시하는 함수 호출
-            displayData(data);
+            displayData(data, containerId);
         } catch (error) {
             console.error('데이터를 불러오는 중 오류가 발생했습니다.', error);
         }
-
     }
 
-    // 페이지 상단으로 스크롤하는 함수
+    // 마커를 추가하는 함수
+    function addMarkers(addressData, shouldRecenter) {
+        clearMarkersAndOverlays();
+        addressData.forEach((data, index) => {
+            searchAndDisplayAddress(data, shouldRecenter && index === 0);
+        });
+    }
+
+
     function scrollToTop() {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     }
 
-    // localStorage에서 검색 결과 복원
     const savedResults = sessionStorage.getItem('searchResults');
     const savedKeyword = sessionStorage.getItem('searchKeyword');
     const savedCollection = sessionStorage.getItem('searchCollection');
@@ -204,7 +190,6 @@ document.addEventListener('DOMContentLoaded', function() {
         const rank = JSON.parse(savedRank);
 
         searchbox.value = keyword;
-        // 복원된 검색 조건에 따라 체크박스 상태 업데이트
         document.querySelectorAll('.search-check').forEach(checkbox => {
             checkbox.checked = collection.includes(checkbox.value);
         });
@@ -220,10 +205,9 @@ document.addEventListener('DOMContentLoaded', function() {
         addMarkers(addresses, true);
     }
 
-    // 검색 버튼 클릭 시 실행되는 이벤트 리스너
     if (searchBtn) {
         searchBtn.addEventListener('click', async function() {
-            if(searchBtn.disabled) {
+            if (searchBtn.disabled) {
                 console.log("서버 통신 중");
                 return;
             }
@@ -233,7 +217,6 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    // 검색어 입력 필드에서 Enter 키 입력 시 실행되는 이벤트 리스너
     if (searchbox) {
         searchbox.addEventListener('keypress', async function(event) {
             if (event.key === 'Enter') {
@@ -248,11 +231,28 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    // 지도 줌 레벨 변경 시 실행되는 이벤트 리스너
     if (typeof map !== 'undefined') {
         map.addListener('zoom_changed', function() {
             zoomLevel = map.getLevel();
             updateOverlaysVisibility();
         });
     }
+
+    
+
+    // 마커와 오버레이를 제거하는 함수
+    function clearMarkersAndOverlays() {
+        if (Array.isArray(markers)) {
+            markers.forEach(marker => marker.setMap(null));
+            markers = [];
+        }
+        if (Array.isArray(overlays)) {
+            overlays.forEach(overlay => overlay.setMap(null));
+            overlays = [];
+        }
+    }
+
+    window.search = search;
+    window.displayData = displayData;
+    window.clearMarkersAndOverlays = clearMarkersAndOverlays;
 });
