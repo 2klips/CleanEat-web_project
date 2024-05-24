238f67c568e7385e7f29949eef150ccfbf3a9211
2klips <axz1420@naver.com>
2024-05-24 13:53:16 +0900

    ## 백업 ##

--- public/main/javascript/mainjs.js
+++ public/main/javascript/mainjs.js
@@ -1,4 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // sessionStorage에서 'introSeen' 값을 확인
+    if (!sessionStorage.getItem('introSeen')) {
+        introScreen.style.display = 'flex'; // 인트로 화면을 보여줍니다
+        container.style.display = 'none'; // 메인 컨텐츠를 숨깁니다
+
+        setTimeout(() => {
+            introScreen.style.display = 'none'; // 인트로 화면을 숨깁니다
+            container.style.display = 'block'; // 메인 컨텐츠를 보여줍니다
+            sessionStorage.setItem('introSeen', 'true'); // sessionStorage에 방문 기록을 저장합니다
+        }, 2300); // 예를 들어 5초 후에 인트로 화면을 숨기고 메인 컨텐츠를 보여줍니다
+    } else {
+        introScreen.style.display = 'none'; // 인트로 화면을 숨깁니다
+        container.style.display = 'block'; // 메인 컨텐츠를 즉시 보여줍니다
+    }
+
     if (performance.navigation.type === 1) {
         localStorage.removeItem('searchResults');
         localStorage.removeItem('searchKeyword');
