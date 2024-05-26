166f793bb205ad9bf09109056553e345be3d4e2f
2klips <axz1420@naver.com>
2024-05-26 21:31:59 +0900

    ## 튜토리얼 페이지 제작 및 인트로 페이지 유지보수 ## - 튜토리얼 페이지 추가   > 첫 시작 시, intro 화면이 나온 후, 튜토리얼 페이지가 나오며, 클릭 시, 메인 화면으로 이동 ( 첫 실행 시에만 나타남 ) - 인트로 페이지 분리해서 제작   > 오류가 너무 많아 intro 페이지도 따로 떼어서 제작
    ** 테스트 하는 법 **
    개발자모드F12 에서 > Application > Local storage에서 introSeen / tutorial Seen 의 Value값 공백으로 수정

--- public/main/javascript/mainjs.js
+++ public/main/javascript/mainjs.js
@@ -1,18 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // sessionStorage에서 'introSeen' 값을 확인
-    if (!sessionStorage.getItem('introSeen')) {
-        introScreen.style.display = 'flex'; // 인트로 화면을 보여줍니다
-        container.style.display = 'none'; // 메인 컨텐츠를 숨깁니다
+    const container = document.getElementById('container');
 
-        setTimeout(() => {
-            introScreen.style.display = 'none'; // 인트로 화면을 숨깁니다
-            container.style.display = 'block'; // 메인 컨텐츠를 보여줍니다
-            sessionStorage.setItem('introSeen', 'true'); // sessionStorage에 방문 기록을 저장합니다
-        }, 2300); // 예를 들어 5초 후에 인트로 화면을 숨기고 메인 컨텐츠를 보여줍니다
+    if (!localStorage.getItem('introSeen')) {
+        window.location.href = 'intro.html';
+    } else if (!localStorage.getItem('tutorialSeen')) {
+        window.location.href = 'tutorial.html';
     } else {
-        introScreen.style.display = 'none'; // 인트로 화면을 숨깁니다
-        container.style.display = 'block'; // 메인 컨텐츠를 즉시 보여줍니다
+        container.style.display = 'block';
     }
+    
+
 
     if (performance.navigation.type === 1) {
         localStorage.removeItem('searchResults');
@@ -26,6 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
         sessionStorage.removeItem('searchRank');
         sessionStorage.removeItem('addresses');
         sessionStorage.removeItem('selectedLocation');
+        sessionStorage.removeItem('tutorialSeen');
     }
 
     const searchBtn = document.getElementById('search-btn');
